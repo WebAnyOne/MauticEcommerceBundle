4ac25f97f04d8518fb1894f2b45b82722fc2af10
Benjamin Lévêque <benjamin@leveque.me>
2022-03-23 17:23:29 +0100

    wip

--- Entity/Transaction.php
+++ Entity/Transaction.php
@@ -5,13 +5,29 @@ namespace MauticPlugin\WebAnyOneMauticPrestashopBundle\Entity;
 class Transaction
 {
     private $contact;
-    private \DateTimeImmutable $createdAt;
-    private ?\DateTimeImmutable $purchaseAt;
 
-    public function __construct($contact, \DateTimeImmutable $createdAt, ?\DateTimeImmutable $purchaseAt)
-    {
+    private string $id;
+    private \DateTimeImmutable $date;
+    private int $price;
+    private int $priceWithTaxes;
+    private int $nbProducts;
+    private $stat;
+
+    public function __construct(
+        $contact,
+        string $id,
+        \DateTimeImmutable $date,
+        int $price,
+        int $priceWithTaxes,
+        int $nbProducts,
+        $stat
+    ) {
         $this->contact = $contact;
-        $this->createdAt = $createdAt;
-        $this->purchaseAt = $purchaseAt;
+        $this->id = $id;
+        $this->date = $date;
+        $this->price = $price;
+        $this->priceWithTaxes = $priceWithTaxes;
+        $this->nbProducts = $nbProducts;
+        $this->stat = $stat;
     }
 }
