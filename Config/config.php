017cd885d6e3e4c5ba24f1edac98dd46ae1e33c2
Benjamin Lévêque <benjamin@leveque.me>
2022-03-23 14:09:27 +0100

    Empty Mautic Plugin

---
+++ Config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => 'WebAnyOne PrestashopBundle ',
+    'description' => 'Retrieve data from Prestashop',
+    'version' => '0.1',
+    'author' => 'elao',
+    'routes' => [],
+    'services' => [],
+];
---
+++ WebAnyOneMauticPrestashopBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WebAnyOne\MauticPrestashopBundle;
+
+use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
+
+class WebAnyOneMauticPrestashopBundle extends AbstractPluginBundle
+{
+
+}
